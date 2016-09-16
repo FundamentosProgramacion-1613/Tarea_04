d630f2b8966a0463e962bacf508a3f4160f73d9c
01372933 <a01372933@itesm.mx>
2016-09-16 04:04:41 +0000

    Add files via upload
    :,(

---
+++ NUMEROSROMANOS.py
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+#Blanca Flor Cálderón Vázquez
+# Lee el número del 1-10 e imprime su equvalente en romano.
+
+
+def transformarNumero(n):
+    x in range (uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez)
+    
+    if n!=x:
+        numeroR="Error"
+   
+    else:
+        
+        
+    return numeroR
+          
+    else :
+    
+
+def main():
+    numero=int(input("ingresa un número del 1-10 en letra"))
+    
+
+main()
