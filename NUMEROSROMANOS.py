6f82e7447be940f098c81945de8b41d1704da0de
01372933 <a01372933@itesm.mx>
2016-09-19 21:55:46 +0000

    Add files via upload
    Aquí ya están los chidos profe :333

--- NUMEROSROMANOS.py
+++ NUMEROSROMANOS.py
@@ -1,24 +1,24 @@
 # encoding: UTF-8
 #Blanca Flor Cálderón Vázquez
 # Lee el número del 1-10 e imprime su equvalente en romano.
-
+#ahora entre 1-20
 
 def transformarNumero(n):
-    x in range (uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez)
-    
-    if n!=x:
-        numeroR="Error"
-   
+    if n<4:
+        romano= "I"*n
+    elif n<6:
+        romano=(5-n)*"I"+"V"
+    elif n<9:
+        romano= "V"+("I"*(n-5))
     else:
-        
-        
-    return numeroR
-          
-    else :
-    
+        romano=(10-n)*"I"+"X"
+    return romano    
 
 def main():
     numero=int(input("ingresa un número del 1-10 en letra"))
+    if numero>0 and numero<11:
+        numeroRomano=transformarNumero(numero)
+        print(numeroRomano)
     
 
 main()
