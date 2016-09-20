92501065a1d97fa69382db8da3802102a133545a
Carlos E. Carbajal Nogués <a01373264@itesm.mx>
2016-09-20 09:50:48 -0500

    Update Descuento.py

--- Descuento.py
+++ Descuento.py
@@ -8,16 +8,16 @@ def calcularDescuento(numpaq): #Función que nos dará el precio con descuento
     if (numpaq <=0):
         res1=str("No compraste ningún paquete")
     elif (numpaq>=10 and numpaq<=19):
-        precio = numpaq*(1500*1.20)
+        precio = numpaq*(1500*.8)
         res1 = ("El precio total será de $ %.2f, se aplicó el 20%%  de descuento" %precio)
     elif(numpaq<=49 and numpaq>=20):
-        precio = numpaq*(1500*1.30)
+        precio = numpaq*(1500*.7)
         res1=("El precio total será de $ %.2f, se aplicó el 30%% de descuento" %precio)
     elif (numpaq>=50 and numpaq<=99):
-        precio = numpaq*(1500*1.4)
+        precio = numpaq*(1500*.6
         res1=("El precio total será de $ %.2f, se aplicó el 40%% de descuento" %precio)
     elif (numpaq>=100):
-        precio = numpaq*(1500*1.5)
+        precio = numpaq*(1500*.5)
         res1=("El precio total será de $ %.2f, se aplicó el 50%% de descuento" %precio)
     else:
         precio = 1500*numpaq
